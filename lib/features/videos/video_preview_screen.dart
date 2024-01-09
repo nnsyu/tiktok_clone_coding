ac397eb9eb82742014ec566d36e4cee778528d65
nnnsyu <nnnsyu@gmail.com>
2024-01-09 18:36:15 +0900

    틱톡클론코딩 #19.8 GallerySaver 까지 수강

---
+++ lib/features/videos/video_preview_screen.dart
@@ -0,0 +1,87 @@
+import 'dart:io';
+
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:gallery_saver/gallery_saver.dart';
+import 'package:video_player/video_player.dart';
+
+class VideoPreviewScreen extends StatefulWidget {
+  final XFile video;
+
+  const VideoPreviewScreen({
+    super.key,
+    required this.video,
+  });
+
+  @override
+  State<VideoPreviewScreen> createState() => _VideoPreviewScreenState();
+}
+
+class _VideoPreviewScreenState extends State<VideoPreviewScreen> {
+  late final VideoPlayerController _videoPlayerController;
+
+  bool _savedVideo = false;
+
+  Future<void> initVideo() async {
+    _videoPlayerController = VideoPlayerController.file(
+      File(
+        widget.video.path,
+      ),
+    );
+
+    await _videoPlayerController.initialize();
+    await _videoPlayerController.setLooping(true);
+    await _videoPlayerController.play();
+
+    setState(() {});
+  }
+
+  @override
+  void dispose() {
+    _videoPlayerController.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    initVideo();
+  }
+
+  Future<void> _saveToGallery() async {
+    if (_savedVideo) {
+      return;
+    }
+
+    await GallerySaver.saveVideo(
+      widget.video.path,
+      albumName: "Tiktok Clone !",
+    );
+
+    _savedVideo = true;
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text('Preview video'),
+        actions: [
+          IconButton(
+            onPressed: _saveToGallery,
+            icon: FaIcon(
+              _savedVideo ? FontAwesomeIcons.check : FontAwesomeIcons.download,
+            ),
+          ),
+        ],
+      ),
+      body: _videoPlayerController.value.isInitialized
+          ? VideoPlayer(_videoPlayerController)
+          : null,
+    );
+  }
+}
--- lib/features/videos/video_recording_screen.dart
+++ lib/features/videos/video_recording_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/videos/video_preview_screen.dart';
 import 'package:tiktok_clone/features/videos/widgets/flash_button.dart';
 
 class VideoRecordingScreen extends StatefulWidget {
@@ -54,6 +55,9 @@ class _VideoRecordingScreenState extends State<VideoRecordingScreen>
     );
 
     await _cameraController.initialize();
+
+    await _cameraController.prepareForVideoRecording();
+
     _flashMode = _cameraController.value.flashMode;
   }
 
@@ -83,13 +87,15 @@ class _VideoRecordingScreenState extends State<VideoRecordingScreen>
       setState(() {});
     });
     _progressAnimationController.addStatusListener((status) {
-      if(status == AnimationStatus.completed)
+      if(status == AnimationStatus.completed) {
         _stopRecording();
+      }
     });
   }
 
   @override
   void dispose() {
+    _progressAnimationController.dispose();
     _cameraController.dispose();
     _buttonAnimationController.dispose();
     super.dispose();
@@ -108,16 +114,36 @@ class _VideoRecordingScreenState extends State<VideoRecordingScreen>
     setState(() {});
   }
 
-  void _startRecording(TapDownDetails _) {
+  Future<void> _startRecording(TapDownDetails _) async {
     print("START RECORDING");
+
+    if(_cameraController.value.isRecordingVideo) {
+      return;
+    }
+
+    await _cameraController.startVideoRecording();
+
     _buttonAnimationController.forward();
     _progressAnimationController.forward();
   }
 
-  void _stopRecording() {
+  Future<void> _stopRecording() async {
     print("STOP RECORDING");
+
+    if(!_cameraController.value.isRecordingVideo) {
+      return;
+    }
+
     _buttonAnimationController.reverse();
     _progressAnimationController.reset();
+
+    final video = await _cameraController.stopVideoRecording();
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => VideoPreviewScreen(video: video),
+      ),
+    );
   }
 
   @override
